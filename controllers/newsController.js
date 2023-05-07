97dd81d9cca7ae215f11057e6e0bb619ecb9ee04
Truong117 <duongbinh117@gmail.com>
2023-05-07 14:48:24 +0700

    update

---
+++ controllers/newsController.js
@@ -0,0 +1,155 @@
+const mongoose = require('mongoose');
+const News = require('../models/newsModel');
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+
+
+
+
+const serverUrl = process.env.SERVER_URL|| 'http://localhost:3000';
+
+const addNews = async (req, res) => {
+    const images = [];
+    if (!req.files.length) {
+      return res.status(400).json({ error: 'No image in the request' });
+    }
+
+    for (let key = 0; key < req.files.length; key++) {
+      const filePath = req.files[key].destination + req.files[key].filename;
+      const imageKey = images.findIndex((img) => Object.keys(img)[0] === `image${key}`);
+      if (imageKey !== -1) {
+        images[imageKey][`image${key}`] = serverUrl+ "/"+ filePath.split(' ').join('-');
+      } else {
+        images.push({ [`image${key}`]: serverUrl+ "/"+ filePath.split(' ').join('-') });
+      }
+    }
+    let news = new News({
+      title: req.body.title,
+      description1: req.body.description1,
+      description2: req.body.description2,
+      description3: req.body.description3,
+      description4: req.body.description4,
+      description5: req.body.description5,
+      images: images
+    });
+  try {
+    await news.save();
+    res.status(202).json({ message: 'Thêm tin tức thành công' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+
+
+const getNewsList = async (req, res) => {
+  try {
+    let perPage = req.query.perPage ?? 20;
+    let page = req.query.page ?? 0;
+    const newsList = await News.find()
+    .limit(perPage)
+    .skip(perPage * page)
+    .sort({
+      createDate: 'desc'
+    })
+    .exec(function(err, data) {
+      News.count().exec(function(err, count) {
+        data = {
+            data: data,
+            perPage: parseInt(perPage),
+            count: count
+        }
+        res.status(200).json(data);
+      })
+    });
+  } catch (error) {         
+    res.status(500).json({ error: error.message });
+  }
+
+
+};
+const getNewsById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const news = await News.findById(id);
+    if (!news) {
+      res.status(404).json({ message: 'Không tìm thấy tin tức' });
+    } else {
+      res.status(200).json(news);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+
+const updateNews = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description1, description2, description3, description4, description5} = req.body;
+    let news = await News.findById(id);
+    if (!news) {
+      res.status(404).json({ message: 'Không tìm thấy tin tức' });
+      return;
+    }
+    const updatedFields = {};
+    if (title) {
+      updatedFields.title = title;
+    }
+    if (description1) {
+      updatedFields.description1 = description1;
+    }
+    if (description2) {
+      updatedFields.description2 = description2;
+    }
+    if (description3) {
+      updatedFields.description3 = description3;
+    }
+    if (description4) {
+      updatedFields.description4 = description4;
+    }
+    if (description5) {
+      updatedFields.description5 = description5;
+    }
+    const images = news.images;
+    if (req.files && req.files.length > 0) {
+      for (let i = 0; i < req.files.length; i++) {
+        const filePath = req.files[i].destination + req.files[i].filename;
+        const imageIndex = req.body.imageIndex[i];
+        images[imageIndex] = { [`image${imageIndex}`]: serverUrl + "/"+ filePath.split(' ').join('-')  };
+      }
+    }
+    news = await News.findByIdAndUpdate(id, { $set: updatedFields, images }, { new: true });
+    res.status(200).json({ message: 'Cập nhật tin tức thành công', news: news });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+  
+  const deleteNews = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await News.findByIdAndDelete(id);
+      res.status(200).json({ message: 'Xóa tin tức thành công' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  module.exports = {
+    addNews,
+    getNewsList,
+    getNewsById,
+    updateNews,
+    deleteNews
+  };
+  
+
+
+
---
+++ controllers/ordersController.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Order = require('../models/ordersModel');
+
+
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json(orders);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const getOrdersById = async (req, res) => {
+    try {
+      const order = await Order.findById(req.params.id);
+      if (order == null) {
+        return res.status(404).json({ message: 'Không tìm thấy đơn đặt hàng' });
+      }
+      res.json(order);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+
+const createOrder = async (req, res) => {
+    const order = new Order({
+      name: req.body.name,
+      phone: req.body.phone,
+      email: req.body.email,
+      content: req.body.content,
+      title: req.body.title
+    });
+    try {
+      const newOrder = await order.save();
+      res.status(201).json(newOrder);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  };
+
+const deleteOrderById = async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+        if (order == null) {
+        return res.status(404).json({ message: 'Không tìm thấy đơn đặt hàng' });
+        }
+        await order.remove();
+        res.json({ message: 'Xóa đơn đặt hàng thành công' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+  
+  
+module.exports = {
+    getAllOrders,
+    createOrder,
+    getOrdersById,
+    deleteOrderById
+  };
